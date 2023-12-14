4218d73daddd0e44a62ef32333ae3e4350433799
Dwi Kurnia Surya <dwi@impstudio.id>
2023-12-14 21:07:35 +0700

    refactor: force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
 
         if($this->app->environment('production')) {
-            URL::forceScheme('http');
+            URL::forceScheme('https');
         }
     }
 }
